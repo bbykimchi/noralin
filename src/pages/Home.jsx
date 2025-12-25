2036a98e0d160f080255358fc5b0aa510424e3d6
Kim Kindvall <kimkin@kth.se>
2025-12-25 18:45:25 +0100

    Basic architecture
    Implemented the front page of clients first draft while awaiting design choices

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,46 @@
 function Home(){
+
+    const buzzwords =[
+        "BRAND",
+        "GROWTH",
+        "IDEATION",
+        "PRODUCTION",
+        "AI STRATEGY",
+        "BRAND"
+    ]
+
     return(
-        <div className="body">
-            <h1>Hello Nora</h1>
-            My name is kim
+        <div>
+            <div className="logo">
+            <img
+            src="src\public\noralin_logo.png"
+            width="250"
+            height="150"
+            />
+            </div>
+            <h1 className="company-name" width>NoraLin Consulting</h1>
+            <div className="buzzword-container">
+                <span className="static-text">Fueling your </span>
+                <div className="scroll-box">
+                    <div className="scroll-content">
+                        {buzzwords.map((word, index) => (
+                            <div key={index} className="scroll-item">
+                                {word}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+            <div className="buzzword-container">
+                <div className="static-text">
+                    with the strength of strategy, spark of creativity and stamina of sport
+                </div>
+            </div>
+            <div className="button-container">
+                <button className="contact-button">Contact</button>
+            </div>
         </div>
+
     )
 }
 export { Home }
